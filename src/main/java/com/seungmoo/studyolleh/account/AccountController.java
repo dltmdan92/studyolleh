005a955c74e8d6f203336250f9bbf7a76a26963b
seungmoo.lee <seungmoo.lee@lotte.net>
2021-02-14 16:43:08 +0900

    회원 가입 뷰

--- src/main/java/com/seungmoo/studyolleh/account/AccountController.java
+++ src/main/java/com/seungmoo/studyolleh/account/AccountController.java
@@ -13,6 +13,9 @@ public class AccountController {
          * 그냥 뷰 띄워줄 경우, 스프링 시큐리티에 의해서 막힘 (login 화면으로 넘어가게 됨)
          * spring security config를 별도로 해줘야 한다.
          */
+        //model.addAttribute("signUpForm", new SignUpForm());
+        // 위의 코드를 아래와 같이 축약 가능하다. (해당하는 객체의 camelCase로 attribute가 생성된다.)
+        model.addAttribute(new SignUpForm());
         return "account/sign-up";
     }
 }
--- src/main/java/com/seungmoo/studyolleh/config/SecurityConfig.java
+++ src/main/java/com/seungmoo/studyolleh/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.seungmoo.studyolleh.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +20,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll() // 얘는 GET 만 허용
                 .anyRequest().authenticated() // 나머지 요청들은 로그인 해야 만 접근 가능
                 ;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // static resource는 Spring Security Filter 적용하지 말 것.
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }
