897e723f428aa8056570ae274a7bb1e57e6f10b3
seungmoo.lee <seungmoo.lee@lotte.net>
2021-02-14 14:51:09 +0900

    계정 도메인 셋팅

---
+++ src/main/java/com/seungmoo/studyolleh/domain/Account.java
@@ -0,0 +1,61 @@
+package com.seungmoo.studyolleh.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+// equals, hashCode 사용할 때 id field만 쓴다.
+// 디폴트로 설정한 경우에, 순환참조로 인한 무한loop가 발생하여 stackoverflow 될 수 있다고 한다.
+@EqualsAndHashCode(of = "id")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    // 기본적으로 String은 varchar(255) 로 매핑된다.
+    @Lob // texttype에 매핑된다 (긴 텍스트 형)
+    @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    // 스터디 생성,등록,수정 이벤트를 어떻게 받을 지?? email or web
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdatedByWeb;
+}
