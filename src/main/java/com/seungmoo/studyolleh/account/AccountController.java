9624181f67931614ba4d8384495dbe83428a370e
seungmoo.lee <seungmoo.lee@lotte.net>
2021-02-15 21:54:39 +0900

    회원 가입 폼 서브밋 검증

--- src/main/java/com/seungmoo/studyolleh/account/AccountController.java
+++ src/main/java/com/seungmoo/studyolleh/account/AccountController.java
@@ -1,11 +1,33 @@
 package com.seungmoo.studyolleh.account;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AccountController {
+
+    private SignUpFormValidator signUpFormValidator;
+
+    /**
+     * API로 signUpForm 데이터 받을 때, Binder 를 설정할 수 있다.
+     * 그리고 이 Binder에 validator를 추가할 수 있다.
+     * signUpForm 객체(객체 타입명 camelCase로 follow 한다.)를 받을 때 (ex POST)
+     * binder가 적용된다.
+     * @param webDataBinder
+     */
+    @InitBinder("signUpForm")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpFormValidator);
+    }
 
     @GetMapping("/sign-up")
     public String signUpForm(Model model) {
@@ -17,5 +39,19 @@ public class AccountController {
         // 위의 코드를 아래와 같이 축약 가능하다. (해당하는 객체의 camelCase로 attribute가 생성된다.)
         model.addAttribute(new SignUpForm());
         return "account/sign-up";
+    }
+
+    /**
+     *
+     * @param signUpForm  @ModelAttribute annotation은 생략 가능
+     * @return
+     */
+    @PostMapping("/sign-up")
+    public String signUpSubmit(@Valid SignUpForm signUpForm, Errors errors) {
+        if (errors.hasErrors()) {
+            return "account/sign-up";
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/seungmoo/studyolleh/account/AccountRepository.java
@@ -0,0 +1,13 @@
+package com.seungmoo.studyolleh.account;
+
+import com.seungmoo.studyolleh.domain.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
+
+// readOnly를 써서 write Lock 방지 토록 한다. (성능)
+@Transactional(readOnly = true)
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    boolean existsByEmail(String email);
+
+    boolean existsByNickname(String nickname);
+}
