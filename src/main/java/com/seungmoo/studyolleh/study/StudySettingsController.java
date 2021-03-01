c12b29a725f70ca1f52116228658d705a009d499
seungmoo.lee <seungmoo.lee@lotte.net>
2021-03-01 20:35:11 +0900

    스터디 배너 이미지 셋팅 기능

--- src/main/java/com/seungmoo/studyolleh/study/StudySettingsController.java
+++ src/main/java/com/seungmoo/studyolleh/study/StudySettingsController.java
@@ -64,6 +64,37 @@ public class StudySettingsController {
         return "redirect:/study/" + study.getEncodedPath() + "/settings/description";
     }
 
+    @GetMapping("/banner")
+    public String studyImageForm(@CurrentUser Account account, @PathVariable String path, Model model) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        model.addAttribute(account);
+        model.addAttribute(study);
+        return "study/settings/banner";
+    }
+
+    @PostMapping("/banner")
+    public String studyImageSubmit(@CurrentUser Account account, @PathVariable String path,
+                                   String image, RedirectAttributes attributes) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.updateStudyImage(study, image);
+        attributes.addFlashAttribute("message", "스터디 이미지를 수정했습니다.");
+        return "redirect:/study/" + study.getEncodedPath() + "/settings/banner";
+    }
+
+    @PostMapping("/banner/enable")
+    public String enableStudyBanner(@CurrentUser Account account, @PathVariable String path) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.enableStudyBanner(study);
+        return "redirect:/study/" + study.getEncodedPath() + "/settings/banner";
+    }
+
+    @PostMapping("/banner/disable")
+    public String disableStudyBanner(@CurrentUser Account account, @PathVariable String path) {
+        Study study = studyService.getStudyToUpdate(account, path);
+        studyService.disableStudyBanner(study);
+        return "redirect:/study/" + study.getEncodedPath() + "/settings/banner";
+    }
+
     private String getPath(String path) {
         return URLEncoder.encode(path, StandardCharsets.UTF_8);
     }
