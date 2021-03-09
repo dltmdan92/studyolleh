9bb2c39f97d4e2f0291bf047b241093c2e09c498
seungmoo.lee <seungmoo.lee@lotte.net>
2021-03-09 21:50:04 +0900

    모임 조회 기능

--- src/main/java/com/seungmoo/studyolleh/event/EventController.java
+++ src/main/java/com/seungmoo/studyolleh/event/EventController.java
@@ -8,6 +8,7 @@ import com.seungmoo.studyolleh.event.validator.EventValidator;
 import com.seungmoo.studyolleh.study.StudyService;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -25,6 +26,7 @@ public class EventController {
     private final EventService eventService;
     private final ModelMapper modelMapper;
     private final EventValidator eventValidator;
+    private final EventRepository eventRepository;
 
     @InitBinder("eventForm")
     public void initBinder(WebDataBinder webDataBinder) {
@@ -53,6 +55,16 @@ public class EventController {
 
         Event event = eventService.createEvent(modelMapper.map(eventForm, Event.class), study, account);
         return "redirect:/study/" + study.getPath() + "/events/" + event.getId();
+    }
+
+    @GetMapping("/events/{id}")
+    public String getEvent(@CurrentUser Account account, @PathVariable String path, @PathVariable Long id,
+                           Model model) {
+        model.addAttribute(account);
+        // findById는 JpaRepository가 기본으로 제공함.
+        model.addAttribute(eventRepository.findById(id).orElseThrow());
+        model.addAttribute(studyService.getStudy(path));
+        return "event/view";
     }
 
 }
