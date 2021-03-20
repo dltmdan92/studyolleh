b24699fb07cdff9ce21affa749e6bcbaf8473483
seungmoo.lee <seungmoo.lee@lotte.net>
2021-03-20 13:35:59 +0900

    알림(notification) 도메인 만듦

---
+++ src/main/java/com/seungmoo/studyolleh/modules/notification/Notification.java
@@ -0,0 +1,34 @@
+package com.seungmoo.studyolleh.modules.notification;
+
+import com.seungmoo.studyolleh.modules.account.Account;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Notification {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    private String link;
+
+    private String message;
+
+    private Boolean checked;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime localDateTime;
+
+    @Enumerated(EnumType.STRING)
+    private NotificationType notificationType;
+}
